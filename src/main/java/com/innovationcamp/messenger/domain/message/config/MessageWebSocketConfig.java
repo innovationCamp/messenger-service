8c25db6e9775ef6d6033df276d5154d0b92f93cf
Kkangjn <jjun1401@naver.com>
2023-08-04 20:12:19 +0900

    Feat : WebSocket 기본 구현

---
+++ src/main/java/com/innovationcamp/messenger/domain/message/config/MessageWebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.innovationcamp.messenger.domain.message.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocket
+public class MessageWebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+    }
+}
