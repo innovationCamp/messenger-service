ceb837f28e5a68f1bc36c50f58b5d8919daad005
KH <106093449+KH-CHOO@users.noreply.github.com>
2023-08-11 14:14:54 +0900

    feat: 예외 발생시 콘솔에서도 확인 가능

--- src/main/java/com/innovationcamp/messenger/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/innovationcamp/messenger/global/exception/GlobalExceptionHandler.java
@@ -13,38 +13,48 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 @Slf4j(topic = "GlobalExceptionHandler")
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ExceptionResponseDto> illegalArgumentExceptionHandler(IllegalArgumentException e) {
-        ExceptionResponseDto responseDto = new ExceptionResponseDto(HttpStatus.BAD_REQUEST,e.getClass().getSimpleName(), e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDto);
+    // General handler for exceptions to reduce code duplication
+    private ResponseEntity<ExceptionResponseDto> createResponse(HttpStatus status, Exception e) {
+        log.error("Exception handled: ", e);  // Logging the entire exception stack trace
+        ExceptionResponseDto responseDto = new ExceptionResponseDto(status, e.getClass().getSimpleName(), e.getMessage());
+        return ResponseEntity.status(status).body(responseDto);
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ExceptionResponseDto> illegalArgumentExceptionHandler(IllegalArgumentException e) {
+        return createResponse(HttpStatus.BAD_REQUEST, e);
+    }
 
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<ExceptionResponseDto> nullPointerExceptionHandler(NullPointerException e) {
-        ExceptionResponseDto responseDto = new ExceptionResponseDto(HttpStatus.BAD_REQUEST,e.getClass().getSimpleName(), e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDto);
+        return createResponse(HttpStatus.BAD_REQUEST, e);
     }
 
     @ExceptionHandler(JsonProcessingException.class)
     public ResponseEntity<ExceptionResponseDto> jsonProcessingExceptionHandler(JsonProcessingException e) {
-        ExceptionResponseDto responseDto = new ExceptionResponseDto(HttpStatus.BAD_REQUEST,e.getClass().getSimpleName(), e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDto);
+        return createResponse(HttpStatus.BAD_REQUEST, e);
     }
 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<ExceptionResponseDto> validationExceptionHandler(ValidationException e) {
-        ExceptionResponseDto responseDto = new ExceptionResponseDto(HttpStatus.BAD_REQUEST,e.getClass().getSimpleName(), e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDto);
+        return createResponse(HttpStatus.BAD_REQUEST, e);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionResponseDto> validationExceptionHandler(MethodArgumentNotValidException e) {
-        ExceptionResponseDto responseDto = new ExceptionResponseDto(HttpStatus.BAD_REQUEST,e.getClass().getSimpleName(), e.getMessage());
+        String message = "Validation error";
+        String detailedErrorMessage = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        ExceptionResponseDto responseDto = new ExceptionResponseDto(HttpStatus.BAD_REQUEST, e.getClass().getSimpleName(), message, detailedErrorMessage);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDto);
     }
 }
