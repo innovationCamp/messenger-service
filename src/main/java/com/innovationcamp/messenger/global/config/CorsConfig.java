6550c0dc398c9c9f9b4a47dccea82325e7e6fbf2
KH <106093449+KH-CHOO@users.noreply.github.com>
2023-08-11 09:41:58 +0900

    refactor : CorsConfig 에서 Filter 사용하도록 변경
    refactor : CorsConfig 에서 Filter 사용하도록 변경

--- src/main/java/com/innovationcamp/messenger/global/config/CorsConfig.java
+++ src/main/java/com/innovationcamp/messenger/global/config/CorsConfig.java
@@ -1,16 +1,24 @@
 package com.innovationcamp.messenger.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:4000")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
-                .allowedHeaders("Content-Type", "Authorization")
-                .allowCredentials(true);
+public class CorsConfig {
+    // CorsFilter Create
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.addExposedHeader("*"); // 헤더로 JWT 토큰 넘겨줄 때 필요
+        source.registerCorsConfiguration("/**",config);
+        return new CorsFilter(source);
     }
 }
