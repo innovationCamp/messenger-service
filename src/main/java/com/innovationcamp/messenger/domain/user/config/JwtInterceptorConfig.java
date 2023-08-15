2dc17a655bd50256aaa38d837c6413b512290613
KH <106093449+KH-CHOO@users.noreply.github.com>
2023-08-15 19:07:33 +0900

    add: 예외 경로 추가

--- src/main/java/com/innovationcamp/messenger/domain/user/config/JwtInterceptorConfig.java
+++ src/main/java/com/innovationcamp/messenger/domain/user/config/JwtInterceptorConfig.java
@@ -23,6 +23,9 @@ public class JwtInterceptorConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 //order 기본값이 0 이라서 지정 안 해도 될 것 같습니다.
                 .excludePathPatterns(
+                        "/", 
+                        // 가끔씩(10분에 한번 정도?) GET : / 요청이 발생하여 토큰 없음 오류 발생함.
+                        // 정확한 이유 모름, 웹소켓이 자동으로 끊어지는 것인지 확인 필요
                         "/api/user/signup",
                         "/api/user/login",
                         "/api/test/login",
