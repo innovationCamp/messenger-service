f3de3e2e8eed2683eb5587a03f923edf0c509dd7
Kkangjn <jjun1401@naver.com>
2023-09-02 16:18:51 +0900

    Feat : GetChannelContent MongoDB로 변경

--- src/main/java/com/innovationcamp/messenger/domain/message/mongodb/repository/MongoChannelContentRepository.java
+++ src/main/java/com/innovationcamp/messenger/domain/message/mongodb/repository/MongoChannelContentRepository.java
@@ -3,5 +3,8 @@ package com.innovationcamp.messenger.domain.message.mongodb.repository;
 import com.innovationcamp.messenger.domain.message.mongodb.entity.MongoChannelContent;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface MongoChannelContentRepository extends MongoRepository<MongoChannelContent, String> {
+    List<MongoChannelContent> findAllByChannelId(Long channelId);
 }
